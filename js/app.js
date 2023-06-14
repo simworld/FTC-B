2f1d2523be824946a6e8e670ac5c2cc5b7978454
Miki Kiki <krawczykmichal1@gmail.com>
2023-06-14 09:57:58 +0100

    upload files

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// get the current year
+const currentYear = new Date().getFullYear();
+
+// update the footer with the current year
+document.getElementById('currentYear').textContent = currentYear;
